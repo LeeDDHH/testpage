3552e9c7fd3832de87ae8ab040ced9c72c31a873
LeeDDHH <niche3600@gamil.com>
2018-08-05 23:19:41 +0900

    change form-like

--- docs/scripts/main.js
+++ docs/scripts/main.js
@@ -6,16 +6,22 @@ window.onload = function() {
         body = '<姓名>\n';
         body += document.getElementById('name').value;
         body += '\n\n';
-        address=document.getElementById('email').value;
+
         subject='サイトからのメールです。'
 
         body += '<お問い合わせ内容>\n';
         body += document.getElementById('inquiry').value;
         body += '\n\n';
 
+        body += '<送信者>\n';
+        address=document.getElementById('email').value;
+        body += address + '\n\n';
+
+        body += '間違いがなければこのままお送りしてください。';
+
         body = body.replace(/\n\r?/g, '%0D%0A');
 
-        location.href = 'mailto:' + address + '?subject=' + subject + '&body=' + body;
+        location.href = 'mailto:vergissmeinnicht24@gmail.com?subject=' + subject + '&body=' + body;
         body, address, subject ='';
     }
 }
