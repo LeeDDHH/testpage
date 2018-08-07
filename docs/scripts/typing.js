c9d19cd8cfed7d2e7b6abecf482f19b3f86f0645
LeeDDHH <niche3600@gamil.com>
2018-08-07 23:01:00 +0900

    delete ityped

--- docs/scripts/typing.js
+++
@@ -1,9 +0,0 @@
-import { init } from 'ityped'
-
-export default function $() {
-
-    const oneElement = document.querySelector('#info')
-
-
-init(oneElement, { showCursor: false, strings: ['Nice', 'One' ] });
-}
